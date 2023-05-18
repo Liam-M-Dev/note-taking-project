02680efc8c8feacc78e6d50c6e996dc9fc0c5e77
Liam-M-Dev <liammassey21@gmail.com>
2023-05-18 18:22:04 +1000

    Enable routing on netlify

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<h1>TODO</h1>} />
-        {/* <Route path="/notes" element={<h1>TODO</h1>} />
+        <Route path="/notes" element={<h1>TODO</h1>} />
         <Route path="/notes/:noteID" element={<h1>TODO</h1>} />
         <Route path="/notes/:noteID/edit" element={<h1>TODO</h1>} />
         <Route path="/notes/searchByWord/:word" element={<h1>TODO</h1>} />
         <Route path="/notes/sort/duedate" element={<h1>TODO</h1>} />
         <Route path="/notes/sort/createddate" element={<h1>TODO</h1>} />
         <Route path="/notes/filter/overdue" element={<h1>TODO</h1>} />
-        <Route path="/notes/filter/done" element={<h1>TODO</h1>} /> */}
+        <Route path="/notes/filter/done" element={<h1>TODO</h1>} />
       </Routes>
     </div>
   );
