5f8a368543e1634ed4eac641146c3b1ed5efa5f1
Liam-M-Dev <liammassey21@gmail.com>
2023-05-19 16:53:40 +1000

    Fix bug in NoteDisplay function

--- src/components/NotesDisplay.jsx
+++ src/components/NotesDisplay.jsx
@@ -1,11 +1,11 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useState } from "react";
 import { useNoteData } from "../contexts/NotesContext";
 
 
 export default function NoteDisplay(props){
 
     const {id} = props;
-    let note = useRef({});
+    let [localNote, setLocalNote] = useState({});
 
     const globalNotesData = useNoteData();
 
@@ -13,23 +13,21 @@ export default function NoteDisplay(props){
     
 
     useEffect(() =>{
-        note.current = globalNotesData.filter(globalSpecificNote => {
-            return globalSpecificNote.id === id;
-        })
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[])
+        setLocalNote(globalNotesData.find(globalSpecificNote => 
+            globalSpecificNote.id === id));
+    },[globalNotesData, id])
 
 
     return(
         <div>
-            <h4>{note.title}</h4>
-                <p>{note.current.description}</p>
-                <p>{note.current.isCompleted ? "COMPLETE" : "NOT YET DONE"}</p>
-                <input type="checkbox" disabled="disabled" value={note.isCompleted} />
-                <h5>Due Date: {new Date(note.current.dueDate).toLocaleString()}</h5>
+            <h4>{localNote.title}</h4>
+                <p>{localNote.description}</p>
+                <p>{localNote.isCompleted ? "COMPLETE" : "NOT YET DONE"}</p>
+                <input type="checkbox" disabled="disabled" value={localNote.isCompleted} />
+                <h5>Due Date: {new Date(localNote.dueDate).toLocaleString()}</h5>
                 {/* <input type="date" readOnly value={note.dueDate} /> */}
 
-                <h5>Created At: {new Date(note.current.createdAtDate).toLocaleString()}</h5>
+                <h5>Created At: {new Date(localNote.createdAtDate).toLocaleString()}</h5>
                 {/* <input type="datetime-local" readOnly value={note.createdAtDate} /> */}
         </div>
     )
